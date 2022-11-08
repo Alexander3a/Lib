98a5ee79d5d6f0e5d428e3d6e84c0500b1ff4a22
alex <Alexander3a00009@gmail.com>
2022-11-08 11:14:12 +0100

    added lib

---
+++ src/de/alex/lib/TypeSerializer.java
@@ -0,0 +1,10 @@
+package de.alex.lib;
+
+public abstract class TypeSerializer {
+    protected abstract String serialize(Object object);
+
+    protected abstract Object deserialize(String serialized);
+
+    @SuppressWarnings("rawtypes")
+    protected abstract Class getType();
+}
