2c2897eba3fe34a505b5ff44b3f18e948a03cd0a
Alex <Alexander3a00009@gmail.com>
2023-04-12 11:11:00 +0200

    added getRedirectUrl

--- src/de/alex/internet/Basic.java
+++ src/de/alex/internet/Basic.java
@@ -49,4 +49,40 @@ public class Basic {
         }
         return output.toString();
     }
+    @SuppressWarnings("HttpUrlsUsage")
+    public static String getRedirectUrl(String urlString, ArrayList<String> property) {
+        try {
+            Boolean https = null;
+            if(urlString.startsWith("http://"))https=false;
+            if(urlString.startsWith("https://"))https=true;
+            String host = Boolean.FALSE.equals(https) ? urlString.split("http://")[1].split("/")[0] : urlString.split("https://")[1].split("/")[0];
+            String port = Boolean.TRUE.equals(https) ? "443": "80";
+            String org_host=host;
+            if(host.contains(":")){
+                port=host.split(":")[1];
+                host=host.split(":")[0];
+            }
+            URL url = new URL(Boolean.TRUE.equals(https) ?"https":"http",host,Integer.parseInt(port),!https ? urlString.replace("http://"+org_host,"") : urlString.replace("https://"+org_host,""));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36");
+            if(property!=null){
+                property.forEach(x-> connection.setRequestProperty(x.split(":")[0],x.replace(x.split(":")[0]+":","")));
+            }
+            connection.setAllowUserInteraction(true);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setInstanceFollowRedirects(false);
+            connection.connect();
+            final int stat = connection.getResponseCode();
+            if (stat > 300 && stat < 307) {
+                final String loc = connection.getHeaderField("Location");
+                return loc;
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
 }
