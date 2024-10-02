fc12e78f17f96db3cac038906a9b58a9a09c7eb2
Alex <alex@0oo0.dev>
2024-10-02 10:32:40 +0200

    added testing PackageConstructor

--- src/de/alex/general/PackageConstructor.java
+++ src/de/alex/general/PackageConstructor.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.*;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
@@ -54,8 +56,20 @@ public class PackageConstructor<T> {
             if(FileSystems.getDefault()==null){
                 throw new RuntimeException("how did we even get to this?????");
             }
-//            List<Path> collect = Files.walk(FileSystems.getFileSystem(this.getClass().getClassLoader().getResource(pkgName.replaceAll("\\.", "/")).toURI()).getPath(pkgName.replaceAll("\\.", "/"))).collect(Collectors.toList());
-            Path root = Paths.get(this.getClass().getClassLoader().getResource("").toURI());
+            Path root = Paths.get(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+            if(root.getFileName().toString().endsWith(".jar")){
+//                System.out.println("[PackageConstructor] ["+new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime())+"] "+"root "+root.toString());
+//            Path start = Paths.get(this.getClass().getClassLoader().getResource(pkgName.replaceAll("\\.", "/")).toURI());
+                URI uri = this.getClass().getClassLoader().getResource(pkgName.replaceAll("\\.", "/")).toURI();
+//            System.out.println("[PackageConstructor] ["+new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime())+"] "+"pain "+uri.toURL());
+//                Path start = Paths.get(uri.toURL().getPath());
+                try(FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap())) {
+//                System.out.println("[PackageConstructor] ["+new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime())+"] "+"system "+root.isAbsolute()+" "+start.isAbsolute());
+//                System.out.println("[PackageConstructor] ["+new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime())+"] "+"start "+start);
+                    List<Path> collect = Files.walk(fileSystem.getPath(pkgName.replaceAll("\\.", "/"))).collect(Collectors.toList());
+                    return collect.stream().map(Path::toString).filter(x -> x.endsWith(".class")).map(x->x.replace(root.getFileSystem().getSeparator(),".").substring(0,x.length()-x.split("\\.")[1].length()-1)).collect(Collectors.toList());
+                }
+           }
             Path start = Paths.get(this.getClass().getClassLoader().getResource(pkgName.replaceAll("\\.", "/")).toURI());
             List<Path> collect = Files.walk(start).collect(Collectors.toList());
             return collect.stream().map(x->root.getParent()!=null ?root.relativize(x): x).map(Path::toString).filter(x -> x.endsWith(".class")).map(x->x.replace(root.getFileSystem().getSeparator(),".").substring(0,x.length()-x.split("\\.")[1].length()-1)).collect(Collectors.toList());
